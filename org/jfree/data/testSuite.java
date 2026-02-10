8efa612a8ce3c0cbf4d10923ec6e145a4785a252
jbeau085 <jbeau085@mtroyal.ca>
2026-02-10 15:34:54 -0700

    first TestSuite added. Probably wrong idk.

---
+++ org/jfree/data/testSuite.java
@@ -0,0 +1,13 @@
+package org.jfree.data;
+import org.junit.platform.suite.api.*;
+
+@Suite
+@SelectClasses({
+  JamesTests.java,
+  BrettsTests.java,
+  KierasTests.java,
+  SunnysTests.java
+})
+public class TestSuite{
+
+}
